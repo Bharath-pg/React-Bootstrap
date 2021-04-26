445a2b47dcb5e2bab33936b5dcf687bc44d5fcf4
unknown <bharathpg1997@gmail.com>
2021-04-26 11:40:57 +0530

    No change

--- src/App.js
+++ src/App.js
@@ -1,32 +1,27 @@
-import React from 'react';
-import './App.css';
-import Buttons from "./components/Buttons";
-import Grids from "./components/Grids";
-import ReactLogo from './logo.svg';
+import React from 'react'
+import './App.css'
+import Buttons from './components/Buttons'
+import Grids from './components/Grids'
+import ReactLogo from './logo.svg'
 function App() {
   return (
-    <div className="text-center bg-transparent"><br />
-      <div class="p-3 mb-2 bg-dark text-white">
-     
-    
-
-        <h3>
-        React with Bootstrap components
-        </h3>
-        
-        </div>
-        <br />
-        <figure class="figure">
-  <img src={ReactLogo} class="figure-img" />
-  <figcaption class="figure-caption">A caption for the above image.</figcaption>
-</figure>
+    <div className='text-center bg-transparent'>
+      <br />
+      <div class='p-3 mb-2 bg-dark text-white'>
+        <h3>React with Bootstrap components</h3>
+      </div>
+      <br />
+      <figure class='figure'>
+        <img src={ReactLogo} class='figure-img' alt='' />
+        <figcaption class='figure-caption'>
+          A caption for the above image.
+        </figcaption>
+      </figure>
       <Buttons />
       <br />
       <Grids />
     </div>
+  )
+}
 
-    
-  );
-};
-
-export default App;
+export default App
